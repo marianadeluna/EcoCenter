f2567e4e6d6b7320ec4ebaad52673c1cdd1e86c4
Mariana De Luna <marianadeluna9@gmail.com>
2019-06-20 23:14:00 -0700

    using jquery, the animation is working. Need to move function to index.js file

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-(function() {
-  var aboutEL = $('div.about'),
-      //this will tell the program when the animation is triggered.
-      //in this case, the content height / 2
-      aboutELOffset = aboutEL.offset().top/2,
-
-      documentEL = $(document);
-
-  documentEL.on('scroll', function() {
-    if(documentEL.scrollTop() > aboutELOffset && aboutEL.hasClass('hidden')) aboutEL.removeClass('hidden');
-  });
-
-
-})();
+//// this function is currently inserted into the index.html file in script
+// (function() {
+//   var aboutEL = $('div.about'),
+//       //this will tell the program when the animation is triggered.
+//       //in this case, the content height / 2
+//       aboutELOffset = aboutEL.offset().top/2,
+//
+//       documentEL = $(document);
+//
+//   documentEL.on('scroll', function() {
+//     if(documentEL.scrollTop() > aboutELOffset && aboutEL.hasClass('hidden')) aboutEL.removeClass('hidden');
+//   });
+//
+//
+// })();
