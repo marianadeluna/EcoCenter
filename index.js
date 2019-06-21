7a1b073a4dbc86392080f8735ba8a44ea86bbdd4
Mariana De Luna <marianadeluna9@gmail.com>
2019-06-20 23:03:56 -0700

    first attempt at scrolling animation

---
+++ index.js
@@ -0,0 +1,14 @@
+(function() {
+  var aboutEL = $('div.about'),
+      //this will tell the program when the animation is triggered.
+      //in this case, the content height / 2
+      aboutELOffset = aboutEL.offset().top/2,
+
+      documentEL = $(document);
+
+  documentEL.on('scroll', function() {
+    if(documentEL.scrollTop() > aboutELOffset && aboutEL.hasClass('hidden')) aboutEL.removeClass('hidden');
+  });
+
+
+})();
