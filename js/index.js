bd532ac43c0214cdc9d1f3f8e4c6a19043f589da
Mariana De Luna <marianadeluna9@gmail.com>
2019-07-29 18:21:30 -0700

    carousel is functional

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ const setSlidePosition = (slide,index) => {
 
 slides.forEach(setSlidePosition);
 
-//function that moves slides to left and right
+//function that moves slides left and right
 const moveSlide = (target, currentSlide, targetSlide) => {
   track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
   currentSlide.classList.remove('current-slide');
@@ -30,6 +30,20 @@ const moveNav = (currentDot, targetDot) => {
   targetDot.classList.add('current-slide');
 }
 
+//function that hides the left and right buttons when appropriate
+const hideArrowButtons = (slides, prevButton, nextButton, targetIndex) => {
+  if(targetIndex === 0) {
+    prevButton.classList.add('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  } else if (targetIndex === slides.length - 1) {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.add('is-hidden');
+  } else {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  }
+}
+
 //click left, move slides to the left
 prevButton.addEventListener('click', e => {
   const currentSlide = track.querySelector('.current-slide');
@@ -38,6 +52,8 @@ prevButton.addEventListener('click', e => {
   const currentDot = dotNav.querySelector('.current-slide');
   const prevDot = currentDot.previousElementSibling;
   moveNav(currentDot, prevDot);
+  const prevIndex = slides.findIndex(slides => slides === prevSlide)
+  hideArrowButtons(slides, prevButton, nextButton, prevIndex);
 });
 
 //click right, move slides to the right
@@ -48,6 +64,8 @@ nextButton.addEventListener('click', e => {
   const currentDot = dotNav.querySelector('.current-slide');
   const nextDot = currentDot.nextElementSibling;
   moveNav(currentDot, nextDot);
+  const nextIndex = slides.findIndex(slides => slides === nextSlide)
+  hideArrowButtons(slides, prevButton, nextButton, nextIndex);
 });
 
 //click navigation indicators, move to that slide
@@ -61,16 +79,5 @@ dotNav.addEventListener('click', e => {
   const targetSlide = slides[targetIndex];
   moveSlide(track, currentSlide, targetSlide);
   moveNav(currentDot, targetDot);
+  hideArrowButtons(slides, prevButton, nextButton, targetIndex);
 });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
